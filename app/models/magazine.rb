3009acfc167b56fdd7597afcaa902e9f3bad8396
ellijs <ellijs@hotmail.com>
2021-08-20 01:16:55 -0400

    Completed

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,12 @@
 class Magazine < ActiveRecord::Base
-  
+    has_many :subscriptions
+    has_many :readers, through: :subscriptions
+
+    def email_list
+        self.readers.map(&:email)
+    end
+
+    def self.most_popular
+        self.max_by{|a| a.subscriptions.count }
+    end
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,17 @@
 class Reader < ActiveRecord::Base
-  
+    has_many :subscriptions
+    has_many :magazines, through: :subscriptions
+
+    def subscribe(magazine, price)
+        Subscription.create(magazine_id: magazine.id, reader_id: self.id, price: price)
+    end
+
+    def total_subscription_price
+        self.subscriptions.sum(:price)
+    end
+
+    def cancel_subscription(magazine)
+       self.subscriptions.where(magazine_id: magazine.id).destroy_all
+    end
 end
+
